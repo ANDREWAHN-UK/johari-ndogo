f6bd826d8439d03afef817f08547a51728c92c1a
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2022-11-05 16:53:42 +0000

    update conn

--- db/conn.php
+++ db/conn.php
@@ -21,14 +21,22 @@ $dsn = "mysql:host = $host; dbname=$db; charset=$charset";
 
 try{
     $pdo = new PDO($dsn, $user, $pass);
-   // echo 'Hello Database'; //Only use this line to confirm the database is connected
+   echo 'Hello Database'; //Only use this line to confirm the database is connected
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//these attributes tell PDO to inform of us error messages
 } 
 catch(PDOException $e) {
     throw new PDOException($e->getMessage());
 }
 
-
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 //after succesfully connecting, then activate crud.php, i.e. create an instance of crud
 //the functions we will use are in the crud.php file
