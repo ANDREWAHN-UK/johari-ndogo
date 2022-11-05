1749bd3edd2fc887204b088f7d5b69649c174e62
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2022-11-05 16:45:34 +0000

    replaced incorrectly uploaded files

--- db/conn.php
+++ db/conn.php
@@ -1,19 +1,19 @@
 <!-- This file is to handle connecting to the databases -->
  <?php
 // below for local connection
-$host = '127.0.0.1';
-$db = "edward";
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+//$host = '127.0.0.1';
+//$db = "edward";
+//$user = 'root';
+//$pass = '';
+//$charset = 'utf8mb4';
 
 //remote database connection:
 
-//$host = 'remotemysql.com';
-//$db = "EfIHr2Rh6H";
-//$user = 'EfIHr2Rh6H';
-//$pass = '3RjWvm9pLh';
-//$charset = 'utf8mb4';
+$host = 'remotemysql.com';
+$db = "EfIHr2Rh6H";
+$user = 'EfIHr2Rh6H';
+$pass = '3RjWvm9pLh';
+$charset = 'utf8mb4';
 
 
 
@@ -21,12 +21,14 @@ $dsn = "mysql:host = $host; dbname=$db; charset=$charset";
 
 try{
     $pdo = new PDO($dsn, $user, $pass);
-    // echo 'Hello Database'; Only use this line to confirm the database is connected
+   // echo 'Hello Database'; //Only use this line to confirm the database is connected
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//these attributes tell PDO to inform of us error messages
 } 
 catch(PDOException $e) {
     throw new PDOException($e->getMessage());
 }
+
+
 
 //after succesfully connecting, then activate crud.php, i.e. create an instance of crud
 //the functions we will use are in the crud.php file
